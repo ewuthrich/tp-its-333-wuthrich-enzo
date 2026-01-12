b9136835dc12f3d907e3a596d2c846ba862b9105
Enzo Wuthrich <ewuthrich68@gmail.com>
2026-01-12 16:15:46 +0100

    Premier commit

--- srcCodeAPIs/myFirstAPI_with_webAPI/app.py
+++ srcCodeAPIs/myFirstAPI_with_webAPI/app.py
@@ -3,8 +3,24 @@ from flask import Flask, jsonify, request
 app = Flask(__name__)
 
 ## EXO1: API GET: renvoyer un helloworld - API end point name: "api/salutation"
+@app.route('/api/salutation', methods=['GET'])
+def salutation():
+    return jsonify(message = "Hello World")
 
 ## EXO2: API POST: renvoyer un nom fourni en parametre - API end point name: "api/utilisateurs"
+
+@app.route('/api/utilisateurs', methods=['GET', 'POST'])
+def utilisateurs():
+    data = request.get_json()
+
+    if not data or 'nom' not in data:
+        return jsonify({"error": "Le champ 'nom' est requis"}), 400
+        
+
+    return jsonify({"nom": data['nom']}), 201
+    
+
+
 
 # to be tested with curl: 
 # >> curl -i -X GET http://localhost:5000/api/salutation
